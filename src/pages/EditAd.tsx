94fe9085ccce8b5d928db87d8c39ac1c5402a24d
Elise <elise.maaiza33@gmail.com>
2024-10-03 11:29:38 +0200

    🔨  Edit ad

--- src/pages/EditAd.tsx
+++ src/pages/EditAd.tsx
@@ -10,10 +10,22 @@ const EditAd = () => {
    const [ad, setAd] = useState({} as AdCardProps);
    const [categories, setCategories] = useState<CategoryProps[]>([]);
 
+   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+      const { name, value } = e.target;
+      setAd((prevAd) => ({
+         ...prevAd,
+         [name]: value,
+      }));
+   };
+
    const handleUpdate = async (e: React.FormEvent) => {
       e.preventDefault();
-      console.log("coucou");
-      
+      const form = e.target as HTMLFormElement;
+      const formData = new FormData(form as HTMLFormElement);
+      const formJson = Object.fromEntries(formData.entries());
+      console.log(formJson);
+      axios.put(`http://localhost:3000/ads/${id}`, formJson);
+      navigate("/");
    }
 
    useEffect(() => {
@@ -26,6 +38,7 @@ const EditAd = () => {
          try {
             const result = await axios.get(`http://localhost:3000/ads/${id}`);
             setAd(result.data);
+            
          } catch (err) {
             console.log("error", err);
          }
@@ -34,59 +47,55 @@ const EditAd = () => {
       fetchData();
    }, [id]);
 
-   return (
-      <>
-         <h1>Modifier {ad.title}</h1>
-         <form method="post" onSubmit={handleUpdate}>
-            <label htmlFor="title">Titre de l'annonce
-               <input className="text-field" type="text" name="title" placeholder="Titre de l'annonce" value={ad.title} required />
-            </label>
-            
-            <br />
+   if (ad) {
 
-            <label htmlFor="description">Description
-               <textarea className="text-field" name="description" id="description" placeholder="Description..." value={ad.description}></textarea>
-            </label>
-            
-            <br />
+      return (
+         <>
+            <h1>Modifier {ad.title}</h1>
+            <form className="form" method="post" onSubmit={handleUpdate}>
+               <label htmlFor="title">Titre de l'annonce
+                  <input className="text-field" type="text" name="title" placeholder="Titre de l'annonce" value={ad.title} onChange={handleChange} required />
+               </label>
+               
+               <label htmlFor="description">Description
+                  <textarea className="text-field" name="description" id="description" placeholder="Description..." value={ad.description} onChange={handleChange}></textarea>
+               </label>
+               
+               <select className="text-field" name="category" id="category" onChange={handleChange}>
+                  <option value="">
+                     {ad.category?.name || "Sélectionnez une catégorie"}
+                  </option>
+                  {categories.map(category => (
+                     <option value={category.id} key={category.id}>{category.name}</option>
+                  ))}
+               </select>
+               
+               <label htmlFor="price">Prix
+                  <input className="text-field" type="number" name="price" min="0" value={ad.price} onChange={handleChange} required />
+               </label>
+               
+               {/* Provisoire pour le test ! */}
+               <label htmlFor="picture">Entrez l'URL de votre image
+                  <input className="text-field" type="text" name="picture" maxLength={2000} value={ad.picture} onChange={handleChange} required />
+               </label>
 
-            <select className="text-field" name="category" id="category">
-               <option value="">Choisissez une catégorie</option>
-               {categories.map(category => (
-                  <option value={category.id} key={category.id}>{category.name}</option>
-               ))}
-            </select>
-            
-            <br />
+               <label htmlFor="location">Localisation
+                  <input className="text-field" type="text" name="location" placeholder="Paris" value={ad.location} onChange={handleChange} required />
+               </label>
+               
+               <label htmlFor="owner">Vendeur
+                  <input className="text-field" type="text" name="owner" placeholder="Votre nom" value={ad.owner} onChange={handleChange} required />
+               </label>
 
-            <label htmlFor="price">Prix
-               <input className="text-field" type="number" name="price" min="0" value={ad.price} required />
-            </label>
-            
-            <br />
+               {/* <label htmlFor="createdAt">Date d'ajout
+                  <input className="text-field" type="date" name="createdAt" value={ad.createdAt} />
+               </label> */}
 
-            {/* Provisoire pour le test ! */}
-            <label htmlFor="picture">Entrez l'URL de votre image
-               <input className="text-field" type="text" name="picture" maxLength={2000} value={ad.picture} required />
-            </label>
-
-            <label htmlFor="location">Localisation
-               <input className="text-field" type="text" name="location" placeholder="Paris" value={ad.location} required />
-            </label>
-            
-            <br />
-            <label htmlFor="owner">Vendeur
-               <input className="text-field" type="text" name="owner" placeholder="Votre nom" value={ad.owner} required />
-            </label>
-
-            <label htmlFor="createdAt">Date d'ajout
-               <input className="text-field" type="date" name="createdAt" value={ad.createdAt} />
-            </label>
-
-            <button type="submit">Modifier</button>
-         </form>
-      </>
-   );
+               <button type="submit">Modifier</button>
+            </form>
+         </>
+      );
+   }
 };
 
 export default EditAd;
